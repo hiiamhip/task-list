057dfde366bee4630c333072106ad944fe232b0b
hiiamhip-laptop <trantuanhiep420@gmail.com>
2025-03-27 17:46:06 +0000

    add, update, delete task: done

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,33 +1,34 @@
 @extends('layouts.app')
 
-@section('title', 'Add task')
+@section('title', 'Edit task')
 
 @section('content')
-    <form method="POST" action="{{ route('tasks.store') }}">
+    <form method="POST" action="{{ route('tasks.update', ['task' => $task->id]) }}">
         @csrf
+        @method('PUT')
         <div>
             <label for="title">Title</label>
-            <input type="text" id="title" name="title" value="{{ old('title') }}">
+            <input type="text" id="title" name="title" value="{{ $task->title }}">
             @error('title')
                 <p>{{ $message }}</p>
             @enderror
         </div>
         <div>
             <label for="description">Description</label>
-            <textarea id="description" name="description" rows="5">{{ old('description') }}</textarea>
+            <textarea id="description" name="description" rows="5">{{ $task->description }}</textarea>
             @error('description')
                 <p>{{ $message }}</p>
             @enderror
         </div>
         <div>
             <label for="long_description">Long description</label>
-            <textarea id="long_description" name="long_description" rows="10">{{ old('long_description') }}</textarea>
+            <textarea id="long_description" name="long_description" rows="10">{{ $task->long_description }}</textarea>
             @error('long_description')
                 <p>{{ $message }}</p>
             @enderror
         </div>
         <div>
-            <button type="submit">Add task</button>
+            <button type="submit">Edit task</button>
         </div>
     </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,9 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
 use \App\Models\Task;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return redirect()->route('tasks.index');
@@ -24,21 +14,27 @@ Route::get('/tasks', function () {
 })->name('tasks.index');
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
-Route::get('/tasks/{id}', function ($id) {
-    return view('show', ['task' => Task::findOrFail($id)]);
+
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', ['task' => $task]);
+})->name('tasks.edit');
+
+Route::get('/tasks/{task}', function (Task $task) {
+    return view('show', ['task' => $task]);
 })->name('tasks.show');
 
-Route::post('/tasks', function (Request $request) {
-    $data = $request->validate([
-        'title' => 'required|max:255',
-        'description' => 'required',
-        'long_description' => 'required',
-    ]);
-    $task = new Task;
-    $task->title = $data['title'];
-    $task->description = $data['description'];
-    $task->long_description = $data['long_description'];
-    $task->save();
-    return redirect()->route('tasks.show', ['id' => $task->id]);
+Route::post('/tasks', function (TaskRequest $request) {
+    $task = Task::create($request->validated());
+    return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task created successfully');
 })->name('tasks.store');
+
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+    $task->update($request->validated());
+    return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task updated successfully');
+})->name('tasks.update');
+
+Route::delete('/tasks/{task}', function (Task $task) {
+    $task->delete();
+    return redirect()->route('tasks.index')->with('success', 'Task deleted successfully');
+})->name('tasks.destroy');
 
